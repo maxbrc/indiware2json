ea6cc22f4ff1fcc5ca4da55be05801be3c097954
maxbrc <max@biaq.de>
2026-09-11 01:45:08 +0200

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package indiware2json converts the XML output from Indiware Unterrichtsplaner
+// into structured, optimized JSON. It can also output a room plan.
+// Currently tested on version 7.
+package indiware2json
